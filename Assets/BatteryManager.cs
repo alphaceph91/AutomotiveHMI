e862ed03ce24fd9dd44a73ec3e14af419f7c08da
alphaceph91 <ahmedsahabaz@gmail.com>
2022-01-15 23:56:59 +0100

    Battery meter added

---
+++ Assets/BatteryManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Collections;
+
+public class BatteryManager : MonoBehaviour
+{
+    [SerializeField] private Image BatteryBar;
+    public TMP_Text displayText;
+    public GameObject alertUI;
+
+    private float currentValue = 100.0f;
+
+    IEnumerator Start()
+    {
+        while (currentValue >= 0.0f)
+        {
+            yield return new WaitForSecondsRealtime(5);
+            UpdateBattery();
+            if (currentValue < 25.0f)
+            {
+                alertUI.gameObject.SetActive(true);
+            }
+        }
+    }
+    void UpdateBattery()
+    {
+        displayText.text = currentValue.ToString() + "%";
+        currentValue -= 1.0f;
+        BatteryBar.fillAmount -= 0.01f;
+    }
+}
